38563a57c9f2e5027624b6a88adf3896156b8e0b
nikilievska <nikilievska02@gmail.com>
2026-05-13 00:25:56 +0200

    fix: ICoffeeRepository

--- BrewPoint/Repositories/Interfaces/ICoffeeRepository.cs
+++ BrewPoint/Repositories/Interfaces/ICoffeeRepository.cs
@@ -6,7 +6,7 @@ namespace BrewPoint.Repositories.Interfaces
     public interface ICoffeeRepository
     {
         Task<IEnumerable<Coffee>> GetAllCoffees();
-        Task<Coffee> GetCoffeeById(int id);
+        Task<Coffee?> GetCoffeeById(int id);
         //Task<IEnumerable<Coffee>> GetCoffiesByType(int coffeeTypeId);
         Task Create(Coffee coffee);
         Task Delete(int id);
